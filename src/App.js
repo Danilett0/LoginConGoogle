09865144655f05a44bd47f18612ca225b5915589
Danilett0 <daniilo.97@hotmail.com>
2023-10-24 23:24:08 -0500

    primera version del proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Login from "./Components/Login";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
+  const [userLog, setUserLog] = useState(false);
+
+  let imgAvatar = localStorage.getItem("Avatar-User") || "";
+  const nameUser = localStorage.getItem("Name-User") || "";
+  const emailUser = localStorage.getItem("Email-User") || "";
+
+  let headerContent;
+
+  if (!userLog) {
+    if (imgAvatar) {
+      headerContent = (
+        <>
+          <img className="AvatarUser" src={imgAvatar} alt="img avatar user" />{" "}
+          <h5>
+            {nameUser} <br /> {emailUser}
+          </h5>
+        </>
+      );
+    } else {
+      headerContent = <Login setUserLog={setUserLog} />;
+    }
+  } else {
+    headerContent = (
+      <>
+        <img className="AvatarUser" src={imgAvatar} alt="img avatar user" />{" "}
+        <h5>
+          {nameUser} <br /> {emailUser}
+        </h5>
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+      <div className="App">
+        <header className="App-header">{headerContent}</header>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
